7b2064477c225c964f4c9d21e3301e562de980b2
Ivan Sadikov <ivan@ahana.io>
2023-02-13 11:20:45 +1300

    fix issues in tpcds extension

--- velox/external/duckdb/tpcds/tpcds-extension.cpp
+++ velox/external/duckdb/tpcds/tpcds-extension.cpp
@@ -339,7 +339,7 @@ void TPCDSExtension::Load(DuckDB &db) {
 	CreateTableFunctionInfo dsdgen_info(dsdgen_func);
 
 	// create the dsdgen function
-	auto &catalog = Catalog::GetSystemCatalog(*con.context);
+	auto &catalog = Catalog::GetCatalog(*con.context);
 	catalog.CreateTableFunction(*con.context, &dsdgen_info);
 
 	// create the TPCDS pragma that allows us to run the query
@@ -5345,27 +5345,27 @@ int mk_zipcode(int nTable, char *dest, int nRegion, char *city) {
 namespace tpcds {
 
 template <class T>
-static void CreateTPCDSTable(ClientContext &context, string schema, string suffix, bool keys, bool overwrite) {
-	auto info = make_unique<CreateTableInfo>();
+static void CreateTPCDSTable(duckdb::ClientContext &context, string schema, string suffix, bool keys, bool overwrite) {
+	auto info = make_unique<duckdb::CreateTableInfo>();
 	info->schema = schema;
 	info->table = T::Name + suffix;
-	info->on_conflict = overwrite ? OnCreateConflict::REPLACE_ON_CONFLICT : OnCreateConflict::ERROR_ON_CONFLICT;
+	info->on_conflict = overwrite ? duckdb::OnCreateConflict::REPLACE_ON_CONFLICT : duckdb::OnCreateConflict::ERROR_ON_CONFLICT;
 	info->temporary = false;
 	for (idx_t i = 0; i < T::ColumnCount; i++) {
-		info->columns.AddColumn(ColumnDefinition(T::Columns[i], T::Types[i]));
+		info->columns.push_back(duckdb::ColumnDefinition(T::Columns[i], T::Types[i]));
 	}
 	if (keys) {
 		vector<string> pk_columns;
 		for (idx_t i = 0; i < T::PrimaryKeyCount; i++) {
 			pk_columns.push_back(T::PrimaryKeyColumns[i]);
 		}
-		info->constraints.push_back(make_unique<UniqueConstraint>(std::move(pk_columns), true));
+		info->constraints.push_back(make_unique<duckdb::UniqueConstraint>(std::move(pk_columns), true));
 	}
-	auto &catalog = Catalog::GetCatalog(context, INVALID_CATALOG);
+	auto &catalog = duckdb::Catalog::GetCatalog(context);
 	catalog.CreateTable(context, std::move(info));
 }
 
-void DSDGenWrapper::CreateTPCDSSchema(ClientContext &context, string schema, string suffix, bool keys, bool overwrite) {
+void DSDGenWrapper::CreateTPCDSSchema(duckdb::ClientContext &context, string schema, string suffix, bool keys, bool overwrite) {
 	CreateTPCDSTable<CallCenterInfo>(context, schema, suffix, keys, overwrite);
 	CreateTPCDSTable<CatalogPageInfo>(context, schema, suffix, keys, overwrite);
 	CreateTPCDSTable<CatalogReturnsInfo>(context, schema, suffix, keys, overwrite);
@@ -5392,7 +5392,7 @@ void DSDGenWrapper::CreateTPCDSSchema(ClientContext &context, string schema, str
 	CreateTPCDSTable<WebSiteInfo>(context, schema, suffix, keys, overwrite);
 }
 
-void DSDGenWrapper::DSDGen(double scale, ClientContext &context, string schema, string suffix) {
+void DSDGenWrapper::DSDGen(double scale, duckdb::ClientContext &context, string schema, string suffix) {
 	if (scale <= 0) {
 		// schema only
 		return;
@@ -5403,7 +5403,7 @@ void DSDGenWrapper::DSDGen(double scale, ClientContext &context, string schema, 
 	// populate append info
 	vector<unique_ptr<tpcds_append_information>> append_info;
 	append_info.resize(DBGEN_VERSION);
-	auto &catalog = Catalog::GetCatalog(context, INVALID_CATALOG);
+	auto &catalog = duckdb::Catalog::GetCatalog(context);
 
 	int tmin = CALL_CENTER, tmax = DBGEN_VERSION;
 
@@ -5411,7 +5411,7 @@ void DSDGenWrapper::DSDGen(double scale, ClientContext &context, string schema, 
 		auto table_def = GetTDefByNumber(table_id);
 		auto table_name = table_def.name + suffix;
 		assert(table_def.name);
-		auto table_entry = catalog.GetEntry<TableCatalogEntry>(context, schema, table_name);
+		auto table_entry = catalog.GetEntry<duckdb::TableCatalogEntry>(context, schema, table_name);
 
 		auto append = make_unique<tpcds_append_information>(context, table_entry);
 		append->table_def = table_def;
@@ -5439,7 +5439,7 @@ void DSDGenWrapper::DSDGen(double scale, ClientContext &context, string schema, 
 			// append happens directly in builders since they dump child tables
 			// immediately
 			if (builder_func((void *)&append_info, i)) {
-				throw Exception("Table generation failed");
+				throw duckdb::Exception("Table generation failed");
 			}
 		}
 	}
@@ -5456,14 +5456,14 @@ uint32_t DSDGenWrapper::QueriesCount() {
 
 string DSDGenWrapper::GetQuery(int query) {
 	if (query <= 0 || query > TPCDS_QUERIES_COUNT) {
-		throw SyntaxException("Out of range TPC-DS query number %d", query);
+		throw duckdb::SyntaxException("Out of range TPC-DS query number %d", query);
 	}
 	return TPCDS_QUERIES[query - 1];
 }
 
 string DSDGenWrapper::GetAnswer(double sf, int query) {
 	if (query <= 0 || query > TPCDS_QUERIES_COUNT) {
-		throw SyntaxException("Out of range TPC-DS query number %d", query);
+		throw duckdb::SyntaxException("Out of range TPC-DS query number %d", query);
 	}
 
 	if (sf == 0.01) {
@@ -5471,7 +5471,7 @@ string DSDGenWrapper::GetAnswer(double sf, int query) {
 	} else if (sf == 1) {
 		return TPCDS_ANSWERS_SF1[query - 1];
 	} else {
-		throw NotImplementedException("Don't have TPC-DS answers for SF %llf!", sf);
+		throw duckdb::NotImplementedException("Don't have TPC-DS answers for SF %llf!", sf);
 	}
 }
 
